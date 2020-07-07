f72a2d9e10c21f8e62cb36ae71b8f2df5618e6bb
AliceRamirez17 <alicemarcelaramirez@gmail.com>
2020-07-07 12:36:51 -0500

    Adding router

--- burger-queer/src/views/LoginPage/LoginView.vue
+++ burger-queer/src/views/LoginPage/LoginView.vue
@@ -4,7 +4,7 @@
         <img src="../../assets/burger-queer-logo.png" class="logo" alt="burger-queer-logo">
       </div>
       <div class="login-choices">
-        <h1 class="title-text">Escoge una opción</h1>
+        <h1 class="title-text">Escoge una<br>opción</h1>
         <button class="role-btn">Mesero/a</button>
         <button class="role-btn">Jefe de cocina</button>
         <button class="role-btn">Administrador</button>

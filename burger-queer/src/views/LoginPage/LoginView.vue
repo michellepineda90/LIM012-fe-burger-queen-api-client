2f651c1ffaa7b9ddb35353a5827f93d7a600181c
AliceRamirez17 <alicemarcelaramirez@gmail.com>
2020-07-08 02:07:53 -0500

    Probando

--- burger-queer/src/views/LoginPage/LoginView.vue
+++ burger-queer/src/views/LoginPage/LoginView.vue
@@ -7,7 +7,7 @@
         <h1 class="title-text">Escoge una<br>opción</h1>
         <button class="role-btn">Mesero/a</button>
         <button class="role-btn">Jefe de cocina</button>
-        <button class="role-btn">Administrador</button>
+        <button class="role-btn">Administrador :)</button>
       </div>
   </div>
 </template>

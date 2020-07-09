911dd2876cefa250f387b533e5da6b79a44a8b47
Michelle Pineda <michelle.pineda.rivas@gmail.com>
2020-07-09 12:53:00 -0600

    adding eslint config

--- burger-queer/src/router/index.js
+++ burger-queer/src/router/index.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Login from '../components/LoginView.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Login from '../components/LoginView.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-  const routes = [
+const routes = [
   {
     path: '/',
     name: 'Login',
-    component: Login
+    component: Login,
   },
   {
     path: '/credentials',
@@ -16,12 +16,12 @@ Vue.use(VueRouter)
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../components/CredentialsView')
-  }
-]
+    component: () => import(/* webpackChunkName: "about" */ '../components/CredentialsView'),
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;

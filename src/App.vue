99890af576c114430fa4ad3e0eb4f958931d4e53
AliceRamirez17 <alicemarcelaramirez@gmail.com>
2020-07-22 11:40:26 -0500

    Adding LoginView component

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <login-view />
+  </div>
+</template>
+
+<script>
+import LoginView from './components/LoginView.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LoginView,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

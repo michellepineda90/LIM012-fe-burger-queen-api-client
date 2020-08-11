d3ea68271e37074d902d8de41e5287d0da7d7f78
Michelle Pineda <michelle.pineda.rivas@gmail.com>
2020-08-11 15:32:59 -0600

    changing navigation guards for auth and users views

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import jwtDecode from 'jwt-decode';
 import Credentials from '../components/CredentialsView';
+
+const USER_ROLE_ADMIN = 'Admin';
+const AUTH_TOKEN_NAME = 'token';
 
 Vue.use(VueRouter);
 
@@ -10,22 +14,45 @@ const routes = [
     name: 'Credentials',
     component: Credentials,
     beforeEnter: (to, from, next) => {
-      if (window.localStorage.getItem('token') != ''){
-        next('/manager/employees')
+      const token = window.localStorage.getItem(AUTH_TOKEN_NAME);
+      if (token !== '') {
+        try {
+          const user = jwtDecode(token);
+          if (user.role === USER_ROLE_ADMIN) {
+            next('/manager/employees');
+          } else {
+            next();
+          }
+        } catch (error) {
+          next();
+        }
+      } else {
+        next();
       }
     }
   },
   {
     path: '/manager/employees',
     name: 'Manager',
-    component: () => import(/* webpackChunkName: "about" */ '../components/ManagerView'),
+    component: () => import(/* webpackChunkName: "Manager" */ '../components/ManagerView'),
     beforeEnter: (to, from, next) => {
-      if (window.localStorage.getItem('token') != ''){
-        next()
+      const token = window.localStorage.getItem(AUTH_TOKEN_NAME);
+      if (token !== '') {
+        try {
+          const user = jwtDecode(token);
+          if (user.role === USER_ROLE_ADMIN) {
+            next();
+          } else {
+            next('/');
+          }
+        } catch (error) {
+          next('/');
+        }
+      } else { 
+        next('/');
       }
-      next('/')
     }
-  },
+  }
 ];
 
 const router = new VueRouter({
